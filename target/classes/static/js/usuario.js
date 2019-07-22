dfc0f34e1ae29b6b69b96dfe3410729566e9abe3
Michel Medeiros <michel.medeiros@hotmart.com>
2019-07-22 07:57:30 -0300

    Module 5

--- target/classes/static/js/usuario.js
+++ target/classes/static/js/usuario.js
@@ -60,3 +60,17 @@ $(document).ready(function() {
     } );	
 	
 });	
+
+$('.pass').keyup(function(){
+	$('#senha1').val() === $('#senha2').val()
+	    ? $('#senha3').removeAttr('readonly')
+	    : $('#senha3').attr('readonly', 'readonly');
+});
+
+
+
+
+
+
+
+
